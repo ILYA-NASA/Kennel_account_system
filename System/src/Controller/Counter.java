4547376127d1adfd87ffceeaf497f48fc64a7c3a
ILYA.NASA <il.nasa@outlook.com>
2023-02-26 17:45:53 +0300

    bug fix

--- System/src/Controller/Counter.java
+++ System/src/Controller/Counter.java
@@ -1,16 +1,16 @@
 ﻿package Controller;
 
-public class Counter implements AutoCloseable{
+public class Counter implements AutoCloseable {
 
     static int sum;
     {
         sum = 0;
     }
 
-    public void add(){
-        sum ++;
+    public void add() {
+        sum++;
     }
-    
+
     @Override
     public void close() {
         System.out.println("Counter closed");
