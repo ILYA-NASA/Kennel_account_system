42708006045a910d366c1ccfae50def8cb6e04a0
ILYA.NASA <il.nasa@outlook.com>
2023-02-26 17:20:49 +0300

    service interface

---
+++ System/src/Services/IRepository.java
@@ -0,0 +1,12 @@
+﻿package Services;
+
+import java.util.List;
+
+public interface IRepository <T> {
+  
+        List <T> getAll();
+        T getById(int id);
+        int create(T item);
+        int update(T item);  
+        void delete (int item);  
+}
---
+++ System/src/Services/PetRepository.java
@@ -0,0 +1,202 @@
+﻿package Services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.time.LocalDate;
+import Model.*;
+
+
+public class PetRepository implements IRepository<Pet> {
+
+    private Creator petCreator;
+    private Statement sqlSt;
+    private ResultSet resultSet;
+    private String SQLstr;
+    
+    public PetRepository() {
+        this.petCreator = new PetCreator();
+    };
+
+    @Override
+    public List<Pet> getAll() {
+        List<Pet> farm = new ArrayList<Pet>();
+        Pet pet;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                sqlSt = dbConnection.createStatement();
+                SQLstr = "SELECT GenusId, Id, PetName, Birthday FROM pet_list ORDER BY Id";
+                resultSet = sqlSt.executeQuery(SQLstr);
+                while (resultSet.next()) {
+
+                    PetType type = PetType.getType(resultSet.getInt(1));
+                    int id = resultSet.getInt(2);
+                    String name = resultSet.getString(3);
+                    LocalDate birthday = resultSet.getDate(4).toLocalDate();
+                    
+                    pet = petCreator.createPet(type, name, birthday);
+                    pet.setPetId(id);
+                    farm.add(pet);
+                }
+                return farm;
+            } 
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }           
+    }
+
+    @Override
+    public Pet getById(int petId) {
+        Pet pet = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+
+                SQLstr = "SELECT GenusId, Id, PetName, Birthday FROM pet_list WHERE Id = ?";
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+                prepSt.setInt(1, petId);
+                resultSet = prepSt.executeQuery();
+
+                if (resultSet.next()) {
+
+                    PetType type = PetType.getType(resultSet.getInt(1));
+                    int id = resultSet.getInt(2);
+                    String name = resultSet.getString(3);
+                    LocalDate birthday = resultSet.getDate(4).toLocalDate();
+
+                    pet = petCreator.createPet(type, name, birthday);
+                    pet.setPetId(id);
+                } 
+                return pet;
+            } 
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public int create(Pet pet) {
+        int rows;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+
+                SQLstr = "INSERT INTO pet_list (PetName, Birthday, GenusId) SELECT ?, ?, (SELECT Id FROM pet_types WHERE Genus_name = ?)";
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+                prepSt.setString(1, pet.getName());
+                prepSt.setDate(2, Date.valueOf(pet.getBirthdayDate())); 
+                prepSt.setString(3, pet.getClass().getSimpleName());
+
+                rows = prepSt.executeUpdate();
+                return rows;
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } 
+    }
+
+    public void train (int id, String command){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                String SQLstr = "INSERT INTO pet_command (PetId, CommandId) SELECT ?, (SELECT Id FROM commands WHERE Command_name = ?)";
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+                prepSt.setInt(1, id);
+                prepSt.setString(2, command);
+
+                prepSt.executeUpdate();
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } 
+    }
+
+    public List<String> getCommandsById (int petId, int commands_type){   
+        
+        // commands type = 1  - получить команды, выполняемые питомцем, 2 - команды, выполнимые животным того рода, к которому относится питомец
+
+        List <String> commands = new ArrayList <>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                if (commands_type == 1){
+                    SQLstr = "SELECT Command_name FROM pet_command pc JOIN commands c ON pc.CommandId = c.Id WHERE pc.PetId = ?";
+                } else {
+                    SQLstr = "SELECT Command_name FROM commands c JOIN Genus_command gc ON c.Id = gc.CommandId WHERE gc.GenusId = (SELECT GenusId FROM pet_list WHERE Id = ?)";
+                }
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+                prepSt.setInt(1, petId);
+                resultSet = prepSt.executeQuery();
+                while (resultSet.next()) {
+                    commands.add(resultSet.getString(1));
+                }
+                return commands;
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } 
+    }
+
+    @Override
+    public int update(Pet pet) {
+        int rows;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                SQLstr = "UPDATE pet_list SET PetName = ?, Birthday = ? WHERE Id = ?";
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+
+                prepSt.setString(1, pet.getName());
+                prepSt.setDate(2, Date.valueOf(pet.getBirthdayDate())); 
+                prepSt.setInt(3,pet.getPetId());
+                
+                rows = prepSt.executeUpdate();
+                return rows;
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } 
+    }
+
+    @Override
+    public void delete (int id) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (Connection dbConnection = getConnection()) {
+                SQLstr = "DELETE FROM pet_list WHERE Id = ?";
+                PreparedStatement prepSt = dbConnection.prepareStatement(SQLstr);
+                prepSt.setInt(1,id);
+                prepSt.executeUpdate();
+            }
+        } catch (ClassNotFoundException | IOException | SQLException ex) {
+            Logger.getLogger(PetRepository.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } 
+    }
+
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/Resources/database.properties")) {
+
+            props.load(fis);
+            String url = props.getProperty("url");
+            String username = props.getProperty("username");
+            String password = props.getProperty("password");
+
+            return DriverManager.getConnection(url, username, password);
+        }
+    }
+}
